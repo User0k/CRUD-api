5dc151084a5824808b4eac5ef62909f0d0570815
Pavel Altov <pavel.altov@yahoo.com>
2025-10-15 15:44:59 +0300

    fix: port, url sanitizer

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,6 @@ const server = createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- src/utils/sanitizeUrl.ts
+++ src/utils/sanitizeUrl.ts
@@ -3,7 +3,9 @@ import { PathType } from '../types';
 
 export function sanitizeUrl(url?: string): PathType | null {
   if (!url || !url.startsWith(baseUrl)) return null;
-  const [, id] = url.split('/');
+
+  const path = url.slice(baseUrl.length);
+  const [, id] = path.split('/');
 
   if (!id) return { type: 'base' };
   return { type: 'id', id };
