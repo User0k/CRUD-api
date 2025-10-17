9be9ba24175a46a86125fea297bbeaaa25d88933
Pavel Altov <pavel.altov@yahoo.com>
2025-10-17 10:36:23 +0300

    refactor: move api main logic with server creation

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,50 @@
-export { apiGet } from './apiGet';
-export { apiPost } from './apiPost';
-export { apiPut } from './apiPut';
-export { apiDelete } from './apiDelete';
+import { createServer } from 'node:http';
+
+import { apiDelete } from './apiDelete';
+import { apiGet } from './apiGet';
+import { apiPost } from './apiPost';
+import { apiPut } from './apiPut';
+import { jsonStringify } from '../utils/jsonStringify';
+import { sanitizeUrl } from '../utils/sanitizeUrl';
+import { StatusCode, HTTPMethod } from '../types/enums';
+
+export const apiServer = createServer((req, res) => {
+  try {
+    const { method, url } = req;
+    const route = sanitizeUrl(url);
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (!route) {
+      res.statusCode = StatusCode.NotFound;
+      res.end(jsonStringify({ message: 'Incorrect api url' }));
+      return;
+    }
+
+    switch (method) {
+      case HTTPMethod.Get:
+        apiGet(res, route!);
+        break;
+      case HTTPMethod.Post:
+        apiPost(req, res);
+        break;
+      case HTTPMethod.Put:
+        apiPut(req, res, route!);
+        break;
+      case HTTPMethod.Delete:
+        apiDelete(res, route!);
+        break;
+      default:
+        res.statusCode = StatusCode.NotFound;
+        res.end(jsonStringify({ message: 'This method is not supported' }));
+    }
+  } catch (error) {
+    console.error(error);
+    res.statusCode = StatusCode.ServerError;
+    res.end(
+      jsonStringify({
+        message: 'Internal server error',
+      }),
+    );
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,8 @@
-import { createServer } from 'http';
-
-import { apiDelete, apiGet, apiPost, apiPut } from './api';
-import { jsonStringify } from './utils/jsonStringify';
-import { sanitizeUrl } from './utils/sanitizeUrl';
 import { PORT } from './constants';
-import { HTTPMethod, StatusCode } from './types/enums';
+import { apiServer } from './api';
 
-const server = createServer((req, res) => {
-  const { method, url } = req;
-  const route = sanitizeUrl(url);
-
-  res.setHeader('Content-Type', 'application/json');
-
-  if (!route) {
-    res.statusCode = StatusCode.NotFound;
-    res.end(jsonStringify({ message: 'Incorrect api url' }));
-    return;
-  }
-
-  switch (method) {
-    case HTTPMethod.Get:
-      apiGet(res, route);
-      break;
-    case HTTPMethod.Post:
-      apiPost(req, res);
-      break;
-    case HTTPMethod.Put:
-      apiPut(req, res, route);
-      break;
-    case HTTPMethod.Delete:
-      apiDelete(res, route);
-      break;
-    default:
-      res.statusCode = StatusCode.ServerError;
-      res.end(jsonStringify({ message: 'This method is not supported' }));
-      break;
-  }
-});
-
-server.listen(PORT, () => {
+apiServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+apiServer.on('error', (err) => console.log(err));
